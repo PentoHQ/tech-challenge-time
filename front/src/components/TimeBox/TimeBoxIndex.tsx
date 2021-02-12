521b352670bbc8c301e3e78dc8f906b04492d6c2
Lily-La-Day <lily.keeling@gmail.com>
2021-02-12 20:12:03 +0000

    date picker working (I think), still no styling, enough for today!

--- front/src/components/TimeBox/TimeBoxIndex.tsx
+++ front/src/components/TimeBox/TimeBoxIndex.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
-
-// import { AuthForm } from "../Auth/Auth";
-import { useHistory } from "react-router-dom";
+import DatePicker from "react-datepicker";
 import Auth from "../../lib/Auth";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import * as moment from "moment";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export const TimeBoxIndex: React.FC = () => {
   const [timeBoxes, setTimeBoxes] = useState([]);
+  const [reset, setReset] = useState(false);
+
+  const [filteredTimeBoxes, setFilteredTimeBoxes] = useState([]);
+
+  const [timePeriod, setTimePeriod] = useState();
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   useEffect(() => {
-    console.log("setting start");
+    setTimePeriod(undefined);
+  }, [reset]);
+
+  useEffect(() => {
     axios
       .get("/api/time-boxes", {
         headers: { Authorization: `Bearer ${Auth.getToken()}` },
@@ -25,32 +36,78 @@ export const TimeBoxIndex: React.FC = () => {
       });
   }, []);
 
-  {
-    console.log(timeBoxes);
-  }
+  useEffect(() => {
+    setFilteredTimeBoxes(undefined);
+  }, [reset]);
+
+  const timeFilter = () => {
+    let timeBoxesFiltered = [...timeBoxes];
+    timeBoxesFiltered = timeBoxesFiltered.filter((timeBox) => {
+      return (
+        moment(timeBox.updated_at).startOf(timePeriod).format() ==
+        moment(selectedDate).startOf(timePeriod).format()
+      );
+    });
+    setFilteredTimeBoxes(timeBoxesFiltered);
+  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "600px",
-        flexDirection: "row",
-        flexWrap: "wrap",
-      }}
-    >
-      {timeBoxes &&
-        timeBoxes.map((timeBox) => (
-          <div
-            style={{
-              height: "150px",
-              width: "150px",
-              border: "2px solid black",
-              display: "flex",
-            }}
-          >
-            {timeBox.created_at}
-          </div>
-        ))}
+    <div>
+      <h3>Filter By..</h3>
+      <div>
+        <button onClick={() => setTimePeriod("day")}>Day</button>
+        <button onClick={() => setTimePeriod("week")}>Week</button>
+        <button onClick={() => setTimePeriod("month")}>Month</button>
+        <button onClick={() => setReset(true)}>Show All</button>
+
+        {timePeriod && (
+          <DatePicker
+            dateFormat="dd/MM/yyyy"
+            selected={selectedDate}
+            onChange={() => timeFilter()}
+          />
+        )}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          width: "600px",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {timeBoxes &&
+          !filteredTimeBoxes &&
+          timeBoxes.map((timeBox) => (
+            <div
+              key={timeBox.name}
+              style={{
+                height: "150px",
+                width: "150px",
+                border: "2px solid black",
+                display: "flex",
+              }}
+            >
+              {timeBox.name}
+              {timeBox.created_at}
+              {timeBox.updated_at ? timeBox.updated_at : "Still Active"}
+            </div>
+          ))}
+        {filteredTimeBoxes &&
+          timeBoxes.map((timeBox) => (
+            <div
+              key={timeBox.name}
+              style={{
+                height: "150px",
+                width: "150px",
+                border: "2px solid black",
+                display: "flex",
+              }}
+            >
+              {timeBox.created_at}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
