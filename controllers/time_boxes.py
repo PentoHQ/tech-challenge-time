a6add525b6d8f04cee20cb12009a090f58ffe0da
Lily-La-Day <lily.keeling@gmail.com>
2021-02-12 10:26:45 +0000

    delete route

--- controllers/time_boxes.py
+++ controllers/time_boxes.py
@@ -22,14 +22,30 @@ def create():
     new_time_box = TimeBox()
     new_time_box.user = g.current_user
     new_time_box.save()
-    db.session.add(new_time_box)
-    db.session.commit()
     return jsonify({'id': str(new_time_box.id)}), 201
 
-
 @api.route('/time-box/<int:time_box_id>', methods=['POST'])
+@secure_route
 def update(time_box_id):
     time_box = TimeBox.query.get(time_box_id)
     time_box.active = False
     time_box.save()
     return time_box_schema.jsonify(time_box), 200
+
+@api.route('/time-boxes', methods=['GET'])
+@secure_route
+def show():
+    user = g.current_user
+    time_boxes = TimeBox.query.select_from(TimeBox).filter(TimeBox.user_id==user.id).all()
+    if not time_boxes:
+        return jsonify({'message': 'not found'}), 404
+    return time_box_schema.jsonify(time_boxes, many=True), 200
+
+@api.route('/time-box/<int:time_box_id>', methods=['DELETE'])
+@secure_route
+def cancel(time_box_id):
+    time_box = TimeBox.query.get(time_box_id)
+    time_box.remove()
+    if not time_box:
+        return jsonify({'message': 'not found'}), 404
+    return jsonify({'message': 'cancelled'}), 200
